dd7ada9b6236f38136f9d3baf49ceda8cb0ac798
Christopher Dunn <christopherdunnlaw@gmail.com>
2019-05-07 18:07:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,28 +4,28 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  puts num1 - num2
+   return num1 - num2
 end
 
 def division(num1, num2)
-  puts num1 / num2
+  return num1 / num2
 end
 
 def multiplication(num1, num2)
-  puts num1 * num2
+  return num1 * num2
 end
 
 def modulo(num1, num2)
-  puts num1 % num2
+  return num1 % num2
 end
 
 def square_root(num)
-  num * num
+  return num * num
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
-  num1 + ((num2 * num3) / num4)
+  return num1 + ((num2 * num3) / num4)
 end
 
 
